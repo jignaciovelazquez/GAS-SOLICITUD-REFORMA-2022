4c9fd9fcbd936718b9b345cc39efe4732dca3b7b
Ignacio Velazquez <jignaciovelazquez@gmail.com>
2022-08-12 18:43:52 -0300

    actualizacion

--- GAS_SOLICITUD_REFORMA_2022.js
+++ GAS_SOLICITUD_REFORMA_2022.js
@@ -32,19 +32,70 @@ document.getElementById("InForFTTH").classList.add("d-block");
 
 //Eventos
 
-
 document.getElementById("GENERAR").addEventListener('click', () => {
-
     document.getElementById("NODO").value = "AZUL";
-
-
 })
-
 document.getElementById("BORRAR").addEventListener('click', () => {
-
     document.getElementById("NODO").value = "";
 })
 
 
 
+document.getElementById("TIPO").addEventListener('change', () => {
+
+    console.log("ENTRO");
+
+    let Tipo = document.getElementById("TIPO").value;
+    if (Tipo == "1") {
+        document.getElementById("NODO").value = "ROJO";
+        ModoHFC();
+
+    }
+    if (Tipo == "2") {
+        ModoHFC();
+    }
+    if (Tipo == "3") {
+        ModoHFC();
+    }
+    if (Tipo == "4") {
+        ModoHFC();
+    }
+    if (Tipo == "5") {
+        ModoHFC();
+    }
+    if (Tipo == "6") {
+        ModoFTTH();
+    }
+    if (Tipo == "7") {
+        ModoFTTH();
+    }
+    if (Tipo == "8") {
+        ModoFTTH();
+    }
+    if (Tipo == "9") {
+        ModoFTTH();
+    }
+    if (Tipo == "10") {
+        ModoFTTH();
+    }
+
+
+})
+
+
 //Funciones
+
+const ModoHFC = () => {
+    document.getElementById("InForHFC").classList.remove("d-none");
+    document.getElementById("InForHFC").classList.add("d-block");
+    document.getElementById("InForFTTH").classList.remove("d-block");
+    document.getElementById("InForFTTH").classList.add("d-none");
+}
+
+const ModoFTTH = () => {
+    document.getElementById("InForHFC").classList.remove("d-block");
+    document.getElementById("InForHFC").classList.add("d-none");
+    document.getElementById("InForFTTH").classList.remove("d-none");
+    document.getElementById("InForFTTH").classList.add("d-block");
+}
+
